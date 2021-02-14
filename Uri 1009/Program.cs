1872143cf597faa10b3d21680adce467880f1930
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-14 18:30:24 -0300

    Add files via upload

---
+++ Uri 1009/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri_1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double salario, totalvendas, p;
+
+
+            Console.WriteLine();
+            Console.Write("Digite o nome do vendedor : ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.Write("Digite o salário do vendedor : ");
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.Write("Digite o montante de vendas do mês : ");
+            totalvendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            p = (double) (15.0/100.0);
+
+            salario = (double)salario + (p * totalvendas);
+
+            Console.WriteLine();
+            Console.Write("Nome do vendedor : "+nome);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("Salário total do vendedor : " + salario.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+
+        }
+    }
+}
